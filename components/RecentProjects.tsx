ce10642e04949dbabfe302c0517a179ec8ea74f7
mehreen hossain chowdhury <mehreenhossain@iut-dhaka.edu>
2025-02-11 17:09:46 +0600

    fix: fix for deployment

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,9 +1,6 @@
 "use client";
 
-import { FaLocationArrow } from "react-icons/fa6";
-
 import { projects } from "@/data";
-import { PinContainer } from "./ui/Pin";
 import  Image  from "next/image";
 
 const RecentProjects = () => {
--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -5,10 +5,6 @@ import { IoCopyOutline } from "react-icons/io5";
 import Lottie from "react-lottie";
 
 import { cn } from "@/lib/utils";
-
-
-import { BackgroundGradientAnimation } from "./GradientBg";
-import GridGlobe from "./GridGlobe";
 import animationData from "@/data/confetti.json";
 import MagicButton from "../MagicButton";
 
